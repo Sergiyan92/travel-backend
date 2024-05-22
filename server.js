161a7b19315ada7164982ac5374f5eea722098a7
Sergiyan92 <sergiyan@ukr.net>
2024-05-22 18:48:48 +0300

    create db

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
-app.listen(3000, () => console.log("Server running"));
+const DB_HOST =
+  "mongodb+srv://Serhii:Dzy84f0skguIzmZQ@cluster0.zcdyxr8.mongodb.net/db_points?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
